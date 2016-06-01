01d6833f4efc7f6f945c32d544ea2b7ad2879ab3
fabrice <byxfab001@myuct.ac.za>
2016-06-01 22:10:58 +0200

    Complete PCL basic structure
    Added datasource via sqlite
    Added Poem model
    Added Sqlite package from frank kruger

---
+++ Poetry/Data/DataSource.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using SQLite;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Poetry
+{
+	public class DataSource
+	{
+		public SQLiteConnection Connection { get; }
+
+		public static string Root { get; set; }
+
+		const string filename = "db.db3";
+
+		public DataSource()
+		{
+			Connection = new SQLiteConnection(Path.Combine(Root, filename));
+
+			Connection.CreateTable<Poem>();
+		}
+
+		#region Implement IDataSource
+		public List<Poem> GetItems()
+		{
+
+			return (from i in Connection.Table<Poem>()
+					select i).ToList();
+
+		}
+
+		public int SaveItem(Poem item)
+		{
+
+			if (item.Id != 0)
+			{
+				Connection.Update(item);
+				return item.Id;
+			}
+
+			return Connection.Insert(item);
+
+		}
+
+		#endregion
+	}
+
+}
+
---
+++ Poetry/Model/Poem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Poetry
+{
+	public class Poem
+	{
+		public int Id { get; set; }
+		public string Author { get; set; }
+		public string Title { get; set; }
+		public string Style { get; set; }
+		public int Lines { get; set; }
+		public string Content { get; set; }
+		public string ImageUrl { get; set; }
+		public string Font { get; set; }
+	}
+}
+
