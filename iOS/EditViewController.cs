03d82597f7ab39152d21772101fbb3ab364b6b21
fabrice <byxfab001@myuct.ac.za>
2016-06-01 23:35:47 +0200

    Added edit page
    Will display list of saved poems

---
+++ iOS/EditViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Poetry.iOS
+{
+    public partial class EditViewController : UIViewController
+    {
+        public EditViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
