136b27506c356d78086e728c38fe8ff5ccde7974
fabrice <byxfab001@myuct.ac.za>
2016-06-02 20:01:29 +0200

    Implemented Cloud server data service

--- Poetry/Service/PoetryService.cs
+++ Poetry/Service/PoetryService.cs
@@ -1,12 +1,41 @@
-﻿using System;
-
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
 
 namespace Poetry
 {
 	public class PoetryService
 	{
+
+		public static MobileServiceClient MobileService = new MobileServiceClient("https://poe-try.azurewebsites.net");
+
+		public List<Poem> Poems { get; set; }
+
 		public PoetryService()
 		{
+			Poems = new List<Poem>();
+
+		}
+
+		public async Task<List<Poem>> GetPoems()
+		{
+			var poems = Poems = await MobileService.GetTable<Poem>().ToListAsync();
+			return poems;
+		}
+
+		public async Task SavePoem(Poem poem)
+		{
+			if (poem.Id != 0)
+			{
+				await MobileService.GetTable<Poem>().UpdateAsync(poem);
+				return;
+			}
+			await MobileService.GetTable<Poem>().InsertAsync(poem);
+		}
+
+		public async Task DeletePoem(Poem poem)
+		{
+			await MobileService.GetTable<Poem>().DeleteAsync(poem);
 		}
 	}
 }
