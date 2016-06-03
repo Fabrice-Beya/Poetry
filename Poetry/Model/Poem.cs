502b6ef7a04282efaa2fd9fffad679b41646c9d7
fabrice <byxfab001@myuct.ac.za>
2016-06-03 17:00:04 +0200

    Added Poem
    Will be stored locally
    Added sqlite net pcl pakage

---
+++ Poetry/Model/Poem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SQLite;
+
+namespace Poetry
+{
+	public class Poem
+	{
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+		public string Author { get; set; }
+		public string Title { get; set; }
+		public string Content { get; set; }
+		public string AudioUrl { get; set; }
+		public int Likes { get; set; }
+		public DateTime DateCreated { get; set;}
+	}
+}
+
