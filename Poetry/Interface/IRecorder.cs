3a7cef9759279acca4d6e0e66a639c24d496ef32
fabrice <byxfab001@myuct.ac.za>
2016-06-04 23:28:31 +0200

    Added Recording/playback view controls

---
+++ Poetry/Interface/IRecorder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Poetry
+{
+	public interface IRecorder
+	{
+		void StartRecording();
+		void StopRecording();
+		void PlayRecord(string Filename);
+		void StopPlay();
+	}
+}
+
