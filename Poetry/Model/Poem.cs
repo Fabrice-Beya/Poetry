36b8c0fcc3c39fc946319ba49e35138e57f02293
fabrice <byxfab001@myuct.ac.za>
2016-06-05 03:24:26 +0200

    Updated poems model for azure cloud syncing

--- Poetry/Model/Poem.cs
+++ Poetry/Model/Poem.cs
@@ -1,18 +1,28 @@
 ﻿using System;
 using SQLite;
+using Newtonsoft.Json;
 
 namespace Poetry
 {
 	public class Poem
 	{
 		[PrimaryKey, AutoIncrement]
+		[Newtonsoft.Json.JsonProperty("id")]
 		public int Id { get; set; }
+		[Newtonsoft.Json.JsonProperty("author")]
 		public string Author { get; set; }
+		[Newtonsoft.Json.JsonProperty("title")]
 		public string Title { get; set; }
+		[Newtonsoft.Json.JsonProperty("content")]
 		public string Content { get; set; }
+		[Newtonsoft.Json.JsonProperty("audiourl")]
 		public string AudioUrl { get; set; }
+		[Newtonsoft.Json.JsonProperty("likes")]
 		public int Likes { get; set; }
+
 		public DateTime DateCreated { get; set;}
+		[Microsoft.WindowsAzure.MobileServices.Version]
+		public string AzureVersion { get; set; }
 	}
 }
 
